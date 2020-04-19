6448165ab7d00d7eecaddb6ff8cd43a850acca66
Vishal Kumar <47864536+TizonX@users.noreply.github.com>
2020-04-20 00:12:41 +0530

    Add files via upload

---
+++ calculator/calculator.js
@@ -0,0 +1,13 @@
+function add()
+{
+        num1 = document.getElementById("ftxt").value;
+        num2 = document.getElementById("stxt").value;
+        document.getElementById("result").innerHTML = num1 + num2;
+}
+
+function sub() 
+{ 
+        num1 = document.getElementById("ftxt").value;
+        num2 = document.getElementById("stxt").value;
+document.getElementById("result").innerHTML = num1 - num2;
+}
